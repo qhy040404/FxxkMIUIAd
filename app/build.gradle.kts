bcff7c7fb4772fa4bf34819473295e133028420d
qhy040404 <qhy040404@163.com>
2023-01-02 20:04:26 +0800

    Convert gradle to gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.qhy040404.fxxkmiuiad"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.qhy040404.fxxkmiuiad"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isCrunchPngs = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro")
+            packagingOptions.resources.excludes += setOf(
+                "DebugProbesKt.bin",
+                "META-INF/*.version"
+            )
+            dependenciesInfo.includeInApk = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("dev.rikka.shizuku:api:12.2.0")
+    implementation("dev.rikka.shizuku:provider:12.2.0")
+}
