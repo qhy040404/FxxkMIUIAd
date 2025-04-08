321bc02dc2a9f08ffead68d5fc02954b88a8bb2d
qhy040404 <qhy040404@163.com>
2025-04-08 11:27:16 +0800

    target 36

--- hidden-api/build.gradle.kts
+++ hidden-api/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 android {
     namespace = "com.qhy040404.fxxkmiuiad.hidden_api"
     compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 }
 
 dependencies {
